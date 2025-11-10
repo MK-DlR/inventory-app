aa3c228ba04d146b2256fcd4ea00a54b8f234ead
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2025-11-10 15:30:37 -0500

    feat: create pool and app file contents

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // app.js
+
+require("dotenv").config(); // MUST BE FIRST!
+
+const express = require("express");
+const app = express();
+const path = require("node:path");
+// require router here
+// const ROUTERNAME = require("./routes/ROUTERNAME");
+
+app.set("views", path.join(__dirname, "views"));
+// register view engine
+app.set("view engine", "ejs");
+
+// middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
+
+// use router for all routes
+// app.use("/", ROUTERNAME);
+
+// listen for requests
+const PORT = process.env.PORT || 3005;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Express app listening on port ${PORT}!`);
+});
---
+++ db/pool.js
@@ -0,0 +1,11 @@
+// db/pool.js
+
+const { Pool } = require("pg");
+
+module.exports = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432"),
+});
